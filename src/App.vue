d82edaf531dd106b4c6690eb857f9bc6634f4bde
michal <michal@axeos.com>
2026-01-11 13:46:12 +0100

    feat: limit page width and change title

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 </script>
 <template>
-  <div class="h-screen w-screen overflow-hidden">
+  <div class="h-screen w-screen overflow-hidden ">
     <div class="w-full h-32 bg-primary-600 flex items-center justify-center">
       <span class="text-5xl font-bold">Szybka Decyzja</span>
     </div>
-    <main class="w-full h-full overflow-y-auto">
+    <main class="max-w-7xl h-full overflow-y-auto mx-auto">
       <RouterView/>
     </main>
   </div>
